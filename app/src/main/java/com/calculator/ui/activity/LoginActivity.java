b797436377941b7335dcdf3709812d56dcac71bc
OctoberCat <valternovoi@gmail.com>
2016-08-22 15:11:37 +0300

    initial commit

--- app/src/main/java/com/calculator/ui/activity/LoginActivity.java
+++ app/src/main/java/com/calculator/ui/activity/LoginActivity.java
@@ -5,7 +5,6 @@ package com.calculator.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
@@ -49,21 +48,18 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "NUm4Q1p2DZEseMVkmh1bhJltY";
     private static final String TWITTER_SECRET = "kzha4TpBvqxkRSNCpCeY8ku6MxpWsxoN9x7MAH6u7ZiZrzLOTk";
-    private static final int RC_SIGN_IN = 9001;
+    private final String TAG = LoginActivity.class.getSimpleName();
 
+    private static final int RC_SIGN_IN = 9001;
     private CallbackManager facebookCallbackManager;
     private TwitterAuthClient twitterAuthClient;
     private GoogleApiClient mGoogleApiClient;
 
     @Bind(R.id.login)
     EditText login;
+
     @Bind(R.id.password)
     EditText password;
-
-    /*@Bind(R.id.login_layout)
-    CoordinatorLayout coordinatorLayout;
-*/
-    private final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +69,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         Fabric.with(this, new Twitter(authConfig));
         twitterAuthClient = new TwitterAuthClient();
         //
-
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
@@ -98,6 +93,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                                             String email = object.getString("email");
                                             String name = object.getString("name");
                                             Log.i(TAG, "onCompleted: facebook name: " + name + " email: " + email);
+
+                                            Intent intent = new Intent(LoginActivity.this, CalculatorActivity.class);
+                                            LoginActivity.this.startActivity(intent);
                                         } catch (JSONException e) {
                                             e.printStackTrace();
                                         }
@@ -146,8 +144,11 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
 
     private boolean isValid() {
-        boolean isValid = true;
         //some validation logic here
+        boolean isValid = true;
+        if (login.getText().toString().isEmpty() || password.getText().toString().isEmpty()) {
+            isValid = false;
+        }
         return isValid;
     }
 
@@ -166,7 +167,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             @Override
             public void success(Result<TwitterSession> result) {
                 Log.i(TAG, "success:  twitter username: " + result.data.getUserName());
-
+                Intent intent = new Intent(LoginActivity.this, CalculatorActivity.class);
+                LoginActivity.this.startActivity(intent);
             }
 
             @Override
@@ -192,7 +194,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
             Log.i(TAG, "handleSignInResult:  Google account email: " + acct.getEmail());
             Log.i(TAG, "handleSignInResult:  Google account name: " + acct.getDisplayName());
-
+            Intent intent = new Intent(LoginActivity.this, CalculatorActivity.class);
+            LoginActivity.this.startActivity(intent);
         }
     }
 
